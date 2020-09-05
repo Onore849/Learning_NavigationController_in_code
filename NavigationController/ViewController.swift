f74f6b6a649546a3f4ef63e72efab1d540f72ae2
Onore849 <rion3376@gmail.com>
2020-09-05 13:08:49 +0900

    first commit

--- NavigationController/ViewController.swift
+++ NavigationController/ViewController.swift
@@ -9,12 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBlue
+        
+        button.setTitle("Go to Nav Contoroller", for: .normal)
+        view.addSubview(button)
+        button.backgroundColor = .white
+        button.setTitleColor(.black, for: .normal)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 52)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    @objc private func  didTapButton() {
+        let rootVC = SecondViewController()
+        let navVC = UINavigationController(rootViewController: rootVC)
+        navVC.modalPresentationStyle = .fullScreen
+        
+        present(navVC, animated: true)
+    
     }
 
-
+    
 }
 
+class SecondViewController: UIViewController {
+    
+    private let button = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed
+        title = "welcome"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Dismiss", style: .plain, target: self, action: #selector(dismissSelf))
+        button.setTitle("Push Another controller", for: .normal)
+        view.addSubview(button)
+        button.backgroundColor = .white
+        button.setTitleColor(.black, for: .normal)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 52)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
+    }
+    
+    @objc private func didTapButton() {
+        
+        let vc = UIViewController()
+        vc.view.backgroundColor = .white
+        
+
+        navigationController?.pushViewController(vc, animated: true)
+        
+        
+    }
+    
+    
+    @objc private func dismissSelf() {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+}
